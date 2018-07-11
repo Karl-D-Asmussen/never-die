a3102300e8a5bff886ce53a6c3356b8bbae04920
Karl D. Asmussen <karl.d.asmussen@gmail.com>
2018-07-11 11:24:46 +0200

    Wed Jul 11 11:24:46 CEST 2018

--- make.zsh
+++ make.zsh
@@ -2,11 +2,15 @@
 
 if [[ ! -d html ]]; then
   mkdir html
+  git add html
 fi
 
 for I in ??.mkdn; do
   J=html/${I%.mkdn}.html
   if [[ ( ! -a $J ) || ( $I -nt $J ) ]]; then
     pandoc -M "title=chapter ${I%.mkdn}" -sH style.html -o $J $I
+    git add $J
   fi
 done
+
+git commit -m "Ran make.zsh"
